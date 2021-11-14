6a6c868167a75e035a574bf3fc288109da36dcef
Bartłomiej Mumot <mumot1999@gmail.com>
2021-11-14 10:59:45 +0100

    download opencsv

--- project1/runMapReduce.sh
+++ project1/runMapReduce.sh
@@ -1,3 +1,4 @@
+https://repo1.maven.org/maven2/com/opencsv/opencsv/5.5.2/opencsv-5.5.2.jar
 hadoop fs -rm -r output
-hadoop jar project.jar AvgSizeStations -libjars opencsv.jar data.csv output
+hadoop jar project.jar AvgSizeStations -libjars opencsv-5.5.2.jar data.csv output
 
