8ab34041aa0dc21afdcbb941f80a9af02dde9491
Bartłomiej Mumot <mumot1999@gmail.com>
2021-11-14 10:56:18 +0100

    fix compile

--- project1/MapReduce/compile.sh
+++ project1/MapReduce/compile.sh
@@ -1,5 +1,5 @@
 wget https://projectlombok.org/downloads/lombok.jar
 mkdir build
 rm src/*Test.java
-javac -cp `hadoop classpath`:lombok.jar src/*.java
+javac -cp `hadoop classpath`:lombok.jar src/*.java -d build
 jar -cvf project.jar -C build/ .
